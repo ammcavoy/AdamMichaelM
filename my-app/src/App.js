3c856501f3b5044a721f427c0162f15164f0796e
adammcavoy <ammcavoy@users.noreply.github.com>
2024-12-04 21:57:07 -0700

    broke react somehow

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate} from "react-router-dom";
 
-function App() {
+function HomePage() {
   return (
     <div>
       <h1>Adam Michael McAvoy</h1>
-      <p>Welcome to My Website!</p>
-      <Button/>
+      <AboutRouteButton/>
+      <PicturesRouteButton/>
     </div>
   );
 }
 
-function Button() {
+function AboutPage() {
   return (
-    <button>I am button</button>
+    <div>
+      <h1>Now Listen to Adam rant about how cool he is.</h1>
+      <HomeRouteButton/>
+      <PicturesRouteButton/>
+    </div>
+  );
+}
+
+function PicturePage() {
+  return (
+    <div>
+      Look at all these cool pictures Adam took
+      <HomeRouteButton/>
+      <AboutRouteButton/>
+    </div>
+    
+  );
+}
+
+
+function AboutRouteButton() {
+  const navigate = useNavigate();
+  return (
+    <button onClick={() => navigate("/about")}>
+      About Adam
+    </button>
+  );
+}
+
+function HomeRouteButton() {
+  const navigate = useNavigate();
+  return (
+    <button onClick={() => navigate("/home")}>
+      Home
+    </button>
+  );
+}
+
+function PicturesRouteButton() {
+  const navigate = useNavigate();
+  return (
+    <button onClick={() => navigate("/pictures")}>
+      Pictures Adam Has Taken
+    </button>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/pictures" element={<PicturePage />} />
+      </Routes>
+    </Router>
   );
 }
 
