21a9ef450ed836c9a02c06f571f5e063693c5b46
adammcavoy <ammcavoy@users.noreply.github.com>
2024-12-04 20:50:35 -0700

    rough draft

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Adam Michael McAvoy</h1>
+      <p>Welcome to My Website!</p>
+      <Button/>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <button>I am button</button>
+  );
+}
+
+export default App;
